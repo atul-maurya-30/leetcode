87461f4727ddaa9fe4224319cb401a9b3a670e8a
kashi-ff <119139143+kashi-ff@users.noreply.github.com>
2024-02-15 03:02:52 +0530

    Time: 1722 ms (5.00%), Space: 30.9 MB (78.64%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,6 +1,23 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        dp=[*nums]
-        for i in range(1,len(nums)):
-            dp[i]=max(nums[i],nums[i]+dp[i-1])
-        return max(dp)
+        def sub(arr,left,right):
+            if left==right:
+                return arr[left]
+            mid=(left+right)//2
+            left_max=sub(arr,left,mid)
+            right_max=sub(arr,mid+1,right)
+            maxim=self.Sum(arr,left,right,mid)
+            return max(maxim,left_max,right_max)
+        return sub(nums,0,len(nums)-1)
+    def Sum(self,arr,left,right,mid):
+        left_sum=float('-inf')
+        curr=0
+        for i in range(mid,left-1,-1):
+            curr+=arr[i]
+            left_sum=max(left_sum,curr)
+        right_sum=float('-inf')
+        curr=0
+        for i in range(mid+1,right+1):
+            curr+=arr[i]
+            right_sum=max(right_sum,curr)
+        return left_sum+right_sum

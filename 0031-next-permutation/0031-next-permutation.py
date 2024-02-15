248946a2b4f826eeec49da1eade3311a7f7cece2
kashi-ff <119139143+kashi-ff@users.noreply.github.com>
2024-02-16 02:37:18 +0530

    Time: 35 ms (95.07%), Space: 16.6 MB (56.09%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -4,17 +4,21 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         l=len(nums)
+        #for 2 no.
         if l<=2:
             return nums.reverse()
+        #for more than 2 no. and check for pointer "p"
         p=l-2
         while p>=0 and nums[p]>=nums[p+1]:
             p-=1
-            
+        #if the element is last
         if p==-1:
             return nums.reverse()
+        #as per use of pointer swap the elements 
         for i in range(l-1,p,-1):
             if nums[p]<nums[i]:
                 nums[p],nums[i]=nums[i],nums[p]
                 break
         nums[p+1:]=reversed(nums[p+1:])
+        return nums
                 

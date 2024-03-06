8c0118928e5efa0a1dde05621df62137de3f32a1
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-03-07 02:07:45 +0530

    Time: 49 ms (35.10%), Space: 17.1 MB (71.81%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string=""
+        for i in s:
+            if i.isalnum()==True:
+                string+=i.lower()
+        return string==string[::-1]
+    

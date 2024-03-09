a76826888ae744f4a5bff91c566e625c4cc5914d
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-03-10 01:57:52 +0530

    Time: 41 ms (35.60%), Space: 16.6 MB (54.30%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,9 +1,9 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        a=""
-        for i in zip(*strs):
-            if len(set(i))==1:
-                a+=i[0]
+        a=""#empty String
+        for i in zip(*strs):#use for itereating elements simultaneously
+            if len(set(i))==1:#use for checking the same elements as if there is mulitple elements then its length is greater than 1
+                a+=i[0]#append the value of a
             else:
                 return a
-        return a
+        return a#return the value of a

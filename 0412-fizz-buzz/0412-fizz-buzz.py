19d84226ab83e28ed771bd35fa241ae3c6ab9ce1
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-10-29 00:31:53 +0530

    Time: 3 ms (32.62%), Space: 12.6 MB (20.48%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        s=["1"]
-        for i in range(2,n+1):
-            if i%3==0 and i%5==0:
-                s.append("FizzBuzz")
-            elif i%3==0:
-                s.append("Fizz")
-            elif i%5==0:
-                s.append("Buzz")
-            else:
-                s.append(str(i))
-        return s
+        return [
+            "FizzBuzz" if i%3==0 and i%5==0 else
+            "Fizz" if i%3==0 else
+            "Buzz" if i%5==0 else
+            str(i)
+            for i in range(1,n+1)
+        ]

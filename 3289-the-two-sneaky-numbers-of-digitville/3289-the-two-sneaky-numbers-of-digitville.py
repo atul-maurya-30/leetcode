916c64c8c73996e93d5b157b0487b15df419fb35
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-03 20:12:37 +0530

    Time: 5 ms (31.93%), Space: 11.7 MB (34.53%) - LeetHub

--- 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        m={}
-        s=[]
+        m={} #dictionary to store count of each no.
+        s=[] #list for storing the output
 
         for num in nums:
-            if num in m:
-                m[num]+=1
+            if num in m: #check if no, is already in the dictionary
+                m[num]+=1 #increment it by 1 if no. is found
             else:
-                m[num]=1
-        for num,c in m.items():
-            if c>1:
-                s.append(num)
-        return s
+                m[num]=1 # initialize the count to 1 if the no. is new
+        for num,c in m.items(): #iterate through the m
+            if c>1: #check if the count of the no. is greater than 1
+                s.append(num) #if correct then add inton list
+        return s #return the list

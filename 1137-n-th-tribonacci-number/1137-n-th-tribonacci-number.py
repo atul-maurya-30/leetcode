6fdcaa1cdba58bff701d2ef6b41a23e30ba9db55
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-06 00:15:02 +0530

    Time: 0 ms (100%), Space: 11.7 MB (7.65%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.py
@@ -1,14 +1,14 @@
-class Solution:
-    def tribonacci(self, n: int) -> int:
-        def tri(n):
-            if n==0:
-                return 0
-            elif n==1 or n==2:
-                return 1
-            a,b,c=0,1,1
-            for _ in range(3,n+1):
-                next_t=a+b+c
-                a,b,c=b,c,next_t
-
-            return c
-        return tri(n)
+class Solution(object):
+    def tribonacci(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if(n<2):
+            return n
+        dp=[0,1,1]
+        for i in range(3,n+1):
+            next_t=dp[0]+dp[1]+dp[2]
+            dp[0],dp[1],dp[2]=dp[1],dp[2],next_t
+        return dp[2]
+        

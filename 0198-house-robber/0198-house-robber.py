c0c97e2915c58fe743da9e26622e1e21adb27de8
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-06 11:30:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,8 +1,18 @@
-class Solution:
-    def rob(self, nums: List[int]) -> int:
-        r,nr=0,0
-        for i in nums:
-            newr=nr+i
-            newnr=max(nr,r)
-            r,nr=newr,newnr
-        return max(r,nr)
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        elif len(nums)==1:
+            return nums[0]
+        prev_n=0
+        prev_r=nums[0]
+        for i in range(0,len(nums)):
+            s=max(prev_r,prev_n+nums[i])
+            prev_n=prev_r
+            prev_r=s
+        return prev_r
+        

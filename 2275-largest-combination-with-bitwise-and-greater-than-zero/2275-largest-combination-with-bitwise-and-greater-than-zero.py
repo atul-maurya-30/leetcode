3d5d981d7c09603614c3de869fbdd81ade44789d
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-08 11:25:32 +0530

    Time: 462 ms (75%), Space: 17.8 MB (100%) - LeetHub

--- 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.py
@@ -4,12 +4,14 @@ class Solution(object):
         :type candidates: List[int]
         :rtype: int
         """
-        m=0
-        b=0
+        m=0  #store maximum count of 1 in any bit position
+        b=0  #it is a counter go from 0 to 23
         while b<24:
-            b_bit=0
-            for a in candidates:
-                b_bit+=(a>>b &1)
-            m=max(m,b_bit)
-            b+=1
-        return m
+            b_bit=0 # counts that how many 1 in the b-th positon
+            for a in candidates: #now for checking the elements in the candidates
+
+                #a>>b shifts the bits of a to the right by b positons
+                b_bit+=(a>>b &1) #using &1 checks for bth pos is 1 not and after that it is add in b_bit by +1
+            m=max(m,b_bit) #it is for checking which bit positon have maximum largest combination
+            b+=1 #checking for next bit by incrementaion
+        return m #maximum largest combination

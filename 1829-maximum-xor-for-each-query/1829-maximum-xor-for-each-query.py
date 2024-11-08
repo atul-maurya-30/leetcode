c5ba7b81e4a341ea4dc15b1a0dfc548728a93f59
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-08 17:51:48 +0530

    Time: 91 ms (28.57%), Space: 28.2 MB (94.12%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -5,15 +5,24 @@ class Solution(object):
         :type maximumBit: int
         :rtype: List[int]
         """
+        # Xor all the elements to get the cumulative XOR
+            # xXor0=0 (different)
+            # xXorx=x (same)
         totalXor=0
         for i in nums:
             totalXor^=i
+
+        # Calculate the max value with maximumBits
         maxK=(1<<maximumBit)-1
 
+        # Calculate the result for each query
         r=[]
         for _ in range(len(nums)):
+
+            #k is given as this first query
             k=totalXor^maxK
             r.append(k)
 
+            # totalXor is as second query for removing the element
             totalXor^=nums.pop()
         return r

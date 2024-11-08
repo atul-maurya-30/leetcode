20302eac5668c9214fd118fe13786716b2aa227f
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-08 18:34:45 +0530

    Time: 62 ms (85.71%), Space: 31.5 MB (41.18%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -35,29 +35,30 @@ class Solution(object):
         #     totalXor^=nums.pop()
         # return r
 
+        
         #third way
-        # maxK=(1<<maximumBit)-1
         # n=len(nums)
-        # r=[0]*n
-        # c=0
+        # r=[]
+        # x=0
+        # for i in range(n):
+        #     x^=nums[i]
+
+        # maxk=((1<<maximumBit)-1) #it works as 2^n-1
 
         # for i in range(n):
-        #     c^=nums[i]
-        #     r[n-i-1]=~c&maxK
+        #     k=x^maxk #this will give the flipped value of Xor i.e. for best k
+        #     r.append(k)
+        #     x=(x^nums[n-1-i])
         # return r
 
 
         #fourth way
+        maxK=(1<<maximumBit)-1
         n=len(nums)
-        r=[]
-        x=0
-        for i in range(n):
-            x^=nums[i]
-
-        maxk=((1<<maximumBit)-1) #it works as 2^n-1
+        r=[0]*n
+        c=0
 
         for i in range(n):
-            k=x^maxk #this will give the flipped value of Xor i.e. for best k
-            r.append(k)
-            x=(x^nums[n-1-i])
+            c^=nums[i]
+            r[n-i-1]=~c&maxK
         return r

02eecc0da27311e6877d5658fcccec2b195c955f
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-08 17:58:52 +0530

    Time: 55 ms (85.71%), Space: 31.6 MB (41.18%) - LeetHub

--- 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
+++ 1829-maximum-xor-for-each-query/1829-maximum-xor-for-each-query.py
@@ -8,25 +8,40 @@ class Solution(object):
         # Xor all the elements to get the cumulative XOR
             # xXor0=0 (different)
             # xXorx=x (same)
-        totalXor=0
-        for i in nums:
-            totalXor^=i
+
+        # totalXor=0
+        # for i in nums:
+        #     totalXor^=i
 
         # Calculate the max value with maximumBits
-        maxK=(1<<maximumBit)-1
+        # maxK=(1<<maximumBit)-1
 
         # Calculate the result for each query
-        r=[]
-        # for _ in range(len(nums)):
+        # r=[]
 
+        # first way
+
+        # for _ in range(len(nums)):
         #     #k is given as this first query
         #     k=totalXor^maxK
         #     r.append(k)
-
         #     # totalXor is as second query for removing the element
         #     totalXor^=nums.pop()
+        
 
-        for i in reversed(nums):
-            r.append(totalXor^maxK)
-            totalXor^=nums.pop()
+        #second way
+        # for i in reversed(nums):
+        #     r.append(totalXor^maxK)
+        #     totalXor^=nums.pop()
+        # return r
+
+        #third way
+        maxK=(1<<maximumBit)-1
+        n=len(nums)
+        r=[0]*n
+        c=0
+
+        for i in range(n):
+            c^=nums[i]
+            r[n-i-1]=~c&maxK
         return r

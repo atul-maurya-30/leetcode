399ae27c29a84f689e1883d328a4412d841f9e75
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-10 18:55:35 +0530

    Time: 1763 ms (58.14%), Space: 35.7 MB (87.97%) - LeetHub

--- 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
+++ 3097-shortest-subarray-with-or-at-least-k-ii/3097-shortest-subarray-with-or-at-least-k-ii.py
@@ -1,28 +1,28 @@
 class Solution:
     def updatevec(self,number,vec,val):
         for i in range(0,32):
-            if (number>>i)&1:
-                vec[i]+=val
+            if (number>>i)&1: #check the ith bit
+                vec[i]+=val  #update the count of set bits at the ith position
 
     def getDecimal(self,vec):
         m=0
         for i in range(0,32):
-            if vec[i]>0:
-                m|=(1<<i)
+            if vec[i]>0:  #if there's any set bit in this position
+                m|=(1<<i)  #set the ith bit in the result
         return m
     def minimumSubarrayLength(self, nums: List[int], k: int) -> int:
         n=len(nums)
-        r=float('inf')
+        r=float('inf') #initialize the minimum subarray length as infinity
         vec=[0]*32
         i,j=0,0
         #vec[i]=total no. of setbits in the ith position
 
         #now we have to follow sliding window technique
         while j<n:
-            self.updatevec(nums[j],vec,1)
+            self.updatevec(nums[j],vec,1) #add current number to the vector
             while i<=j and self.getDecimal(vec)>=k:
-                r=min(r,j-i+1)
-                self.updatevec(nums[i],vec,-1)
-                i+=1
-            j+=1
-        return r if r!=float('inf') else -1
+                r=min(r,j-i+1) #update the minimum length
+                self.updatevec(nums[i],vec,-1) #remove the leftmost number
+                i+=1 #shrink the window from the left
+            j+=1 #shrink the window from right
+        return r if r!=float('inf') else -1 #return the result or -1 if no subarray found

6b5348a1005446c2ac9dfcc9d07f6b8a176574f3
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-11 18:49:10 +0530

    Time: 498 ms (47.97%), Space: 29.6 MB (37.1%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -1,10 +1,12 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
+        
+        #it slightly uses the concept of two sums problem
         c=0
-        seen={}
+        seen={} #making hashmap
         for num in nums:
-            t=k-num
-            if t in seen and seen[t]>0 :
+            t=k-num #to find second element
+            if t in seen and seen[t]>0 : #checking for the condition
                 c+=1
                 seen[t]-=1
             else:

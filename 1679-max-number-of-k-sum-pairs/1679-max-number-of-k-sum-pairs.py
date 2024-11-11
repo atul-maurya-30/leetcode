f0ceb06fabe6f6738945b5ae02d45dcb0ff4ccfc
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-11 18:54:26 +0530

    Time: 491 ms (66.84%), Space: 29.6 MB (37.1%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.py
@@ -6,12 +6,14 @@ class Solution:
         seen={} #making hashmap
         for num in nums:
             t=k-num #to find second element
-            if t in seen and seen[t]>0 : #checking for the condition
-                c+=1
-                seen[t]-=1
+
+            #checking for the condtion
+            if t in seen and seen[t]>0 : #if second element in seen and also if second element in seen have a value greater than 0
+                c+=1 #increase the counter
+                seen[t]-=1 #and also decrease the counter of that second element in seen
             else:
-                if num in seen:
-                    seen[num]+=1
-                else:
+                if num in seen: #if present then incraese by 1
+                    seen[num]+=1 
+                else: #else give a value to 1
                     seen[num]=1
-        return c
+        return c #return the counter

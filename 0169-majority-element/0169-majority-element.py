ec5b0d0c09c31b0207aba859092018f615ab5447
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-12 20:25:43 +0530

    Time: 13 ms (23.51%), Space: 18.3 MB (36.23%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,12 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        d={} #dictionary
+        m=0 #maximum count
+        c=0 #Max Count
+
+        for n in nums:
+            d[n]=1+d.get(n,0)
+            if d[n]>c:
+                m=n
+                c=d[n]
+        return m

441eee097395ccd9bf97dad7be7735e98769663d
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-12 16:08:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
@@ -1,26 +1,33 @@
 class Solution:
     def maximumBeauty(self, items: List[List[int]], queries: List[int]) -> List[int]:
+        #it is a straight forward problem as a easy level
+        #sort the items by price
         items.sort()
+        
+        #compute new maximum beauty for each price
         m=[]
         c=0
-
-        for p,b in items:
+        for p,b in items: #p= price
+                          #b= beauty
             c=max(c,b)
             m.append((p,c))
 
-
+        #binary search function for finding max beauty fir price<=query
         def binary(query:int) ->int:
-            l,r=0,len(m)-1
-            d=0
+            l,r=0,len(m)-1 #l=left ; r=right
+            d=0 #default value if no item found
 
             while l<=r:
-                mid=(l+r)//2
-                p,b=m[mid]
-                if p<=query:
-                    d=b
-                    l=mid+1
+                mid=(l+r)//2 #for finding mid value
+
+                #unpacking the data of m, which having max values of beauty 
+                p,b=m[mid] #p=price ; b= beauty
+
+                if p<=query: #check for prices which is less then queries
+                    d=b #update default value to the beautyof the item
+                    l=mid-1 
                 else:
-                    r=mid-1
+                    r=mid+1
             return d
 
         r=[]

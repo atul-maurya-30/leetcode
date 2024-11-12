e8f5cac43ce2345c0e2adb5100442a2a57e52805
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-12 15:52:47 +0530

    Time: 249 ms (48.78%), Space: 65.8 MB (63.44%) - LeetHub

--- 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
+++ 2070-most-beautiful-item-for-each-query/2070-most-beautiful-item-for-each-query.py
@@ -8,13 +8,22 @@ class Solution:
             c=max(c,b)
             m.append((p,c))
 
+
+        def binary(query:int) ->int:
+            l,r=0,len(m)-1
+            d=0
+
+            while l<=r:
+                mid=(l+r)//2
+                p,b=m[mid]
+                if p<=query:
+                    d=b
+                    l=mid+1
+                else:
+                    r=mid-1
+            return d
+
         r=[]
         for q in queries:
-            m_b=0
-            for p,b in m:
-                if p<=q:
-                    m_b=b
-                else:
-                    break
-            r.append(m_b)
+            r.append(binary(q))
         return r

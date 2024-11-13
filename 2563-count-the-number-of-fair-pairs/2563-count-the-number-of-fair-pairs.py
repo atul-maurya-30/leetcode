9ac5b8039fd31a7dfe61fefaf9b8aac97b4a46c0
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-13 17:56:51 +0530

    Time: 115 ms (78.57%), Space: 30.6 MB (71.63%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.py
@@ -1,14 +1,21 @@
 class Solution:
     def countFairPairs(self, nums: List[int], lower: int, upper: int) -> int:
+        #if we sort this array then it will not affect the given problem
         nums.sort()
+
+        # we make this function for overcoming the problem of redundancy of writing the given code
         def counting(final):
-            c=0
-            l=0
-            r=len(nums)-1
-            while l<r:
-                if nums[l]+nums[r]<=final:
-                    c+=(r-l)
-                    l+=1
-                else: r-=1
-            return c
-        return counting(upper)-counting(lower-1)
+            c=0 #counter for counting elements
+            l=0 #left pointer
+            r=len(nums)-1 #right pointer
+            while l<r: #loop start
+                if nums[l]+nums[r]<=final: #for checking the conditon of given problem as
+                                            # it is given that lower<=nums+nums[j]<=upper
+
+        #so we use a concept of line system as we want a range between upper and lower
+                    c+=(r-l)  # we use this becuase we have to count the elements in the given range
+                    l+=1 #after that we have to increment left pointer
+                else: r-=1 #else we have to decrement rightb pointer
+            return c #it will give the total counts of uppper and lower 
+        return counting(upper)-counting(lower-1) # as lower is also included in the condition so we use lower-1
+        #and return the result

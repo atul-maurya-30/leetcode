a8142f447badb90e42f27149aec2dec69b38dcf0
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-14 20:10:36 +0530

    Time: 28 ms (88.59%), Space: 26 MB (63.43%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -3,10 +3,13 @@ class Solution:
         min_p=float("inf")
         max_p=0
 
+#like we use min or max
         for price in prices:
-            if price<min_p:
-                min_p=price
-            pro=price-min_p
-            if pro>max_p:
-                max_p=pro
+            if price<min_p: 
+                min_p=price #update the min value
+
+            pro=price-min_p #update the profit
+
+            if pro>max_p: 
+                max_p=pro #update the max value
         return max_p

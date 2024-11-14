471696ca3782017528fc515dbff651b1efbb8523
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-14 19:11:54 +0530

    Time: 594 ms (61.54%), Space: 28.6 MB (73.44%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.py
@@ -0,0 +1,31 @@
+class Solution:
+
+    '''this function is used as for checking the limit of max items per person to mid'''
+    def poss(self,mid,quantities,n):
+
+        # check for each quantities of each product by using mid that it fulfil the store limit or not
+        s=0
+        for i in quantities:
+            s+= (i+(mid-1))//mid #for adding the ciel value it is the alternative approach
+            if s>n: #agar total stores jo hai wo jyada ho jaye available se to true kardo 
+                return True # aur iska matlb ye hai ki current value jyada bada hai , aur hum distribute nhi kar skte hai
+       
+        return s>n #aur agar required stores kam ho jaye ya barabar ho jaye to false kardo
+
+    def minimizedMaximum(self, n: int, quantities: List[int]) -> int:
+        l=1 #start value of pointer
+        r=max(quantities) #set r for max value of quantities as for final value of pointer
+        
+        #use binary search
+        while(l<r):
+            #calculate mid value, that if store getting more items or not,if yes then increase lower bound
+            mid=(l+r)//2
+            if self.poss(mid,quantities,n):
+                l=mid+1
+
+            #if not then decrease the upper bound
+            else:
+                r=mid
+
+        #at the end l is the maximized maximum no. of items per store 
+        return l

073cc0eb35f0f8272223e962390c03450a6100b1
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-14 19:39:37 +0530

    Time: 0 ms (100%), Space: 17.9 MB (29.01%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,10 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen={}
         for i,num in enumerate(nums):
-            if target-num in seen:
+            if target-num in seen: #checking for 2nd pair no.
                 return ([seen[target-num],i])
             elif num not in seen:
-                seen[num]=i
+                seen[num]=i #if not in the dictionary then add element to dictionary with index value i
         
                     
               

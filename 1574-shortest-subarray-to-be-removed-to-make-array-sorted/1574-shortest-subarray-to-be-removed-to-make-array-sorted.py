b92e06ccddcaa154a7b824f64017b90b57ad99c8
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-15 17:28:17 +0530

    Time: 7 ms (100%), Space: 30.4 MB (90.65%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,23 +1,27 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
-        n=len(arr)
+        n=len(arr) #length of array
 
-        #phase-1 find the jth pointer from the right side
-        r=n-1
+        #phase-1 find the rightmost pointer "r"  where non decreasing sub array ends
+        r=n-1 # r=right
         while r>0 and arr[r]>=arr[r-1]:
             r-=1
         
-        l=0
-        res=r #we remove all elements left to index r because index r to n-1 are sorted
+        l=0 # l=left
+        res=r #initialize result as by removing everything upto "r"
 
-        #phase-2 start finding correct left and right pointers and find delted elements=r-l-1
+        #phase-2 start finding correct left and right pointers and find deleted elements=r-l-1
 
-        while(l<r and (l==0 or arr[l]>=arr[l-1])): #for finding array in incraseing order
-            #arr[r]<arr[l]
+        while(l<r and (l==0 or arr[l]>=arr[l-1])): 
+        #for finding array in increasing order
+            #move "r" right to find arr[r]<arr[l]
+
             while r<n and arr[l]>arr[r]:
                 r+=1
             
-            #we found correct right pos
+            #we found correct right position so update left pointer
             res=min(res,r-l-1)
-            l+=1
+            l+=1 #move "l" to right
+
+        #return the shortest removed subarray length 
         return res

7d4f6b4777d8e1d7a0908672f9ee071dd054fec2
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-16 22:11:12 +0530

    Time: 87 ms (55.21%), Space: 26.5 MB (71.22%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.py
@@ -1,17 +1,25 @@
-from collections import deque
+# from collections import deque
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
-        a=deque()
-        s=0
-        max_av=float('-inf')
+        # a=deque()
+        # s=0
+        # max_av=float('-inf')
 
-        for i in range(len(nums)):
-            a.append(nums[i])
-            s+=nums[i]
+        # for i in range(len(nums)):
+        #     a.append(nums[i])
+        #     s+=nums[i]
 
-            if len(a)>k:
-                s-=a.popleft()
+        #     if len(a)>k:
+        #         s-=a.popleft()
             
-            if len(a)==k:
-                max_av=max(max_av,s/k)
+        #     if len(a)==k:
+        #         max_av=max(max_av,s/k)
+        # return max_av
+
+        s=sum(nums[:k])
+        max_av=s/k
+
+        for i in range(k,len(nums)):
+            s+=nums[i]-nums[i-k]
+            max_av=max(max_av,s/k)
         return max_av

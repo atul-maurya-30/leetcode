40e6615d9d15038ceca0072db73f300d7fe970c6
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-16 21:33:25 +0530

    Time: 59 ms (17.55%), Space: 16.8 MB (55.71%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.py
@@ -4,13 +4,23 @@ class Solution:
         m=[]
         l=0
         r=k
-        while r<n+1:
-            j=nums[l:r]
-            if j==sorted(j) and all(j[i]+1==j[i+1] for i in range(len(j)-1)):
-                m.append(max(j))
+        # while r<n+1:
+        #     j=nums[l:r]
+        #     if j==sorted(j) and all(j[i]+1==j[i+1] for i in range(len(j)-1)):
+        #         m.append(max(j))
+        #     else:
+        #         m.append(-1)
+        #     l+=1
+        #     r+=1
+        # return m                
+
+
+        while r<=n:
+            if nums[l:r]==sorted(nums[l:r]) and all(nums[i]+1==nums[i+1] for i in range(l,r-1)):
+                m.append(max(nums[l:r]))
             else:
                 m.append(-1)
             l+=1
             r+=1
-        return m                
+        return m
 

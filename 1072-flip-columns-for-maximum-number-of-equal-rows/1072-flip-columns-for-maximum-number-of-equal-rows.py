a55b54fdc74fcc79785669fbcd8e050b8acbc815
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-22 16:37:17 +0530

    Time: 47 ms (82.41%), Space: 19.5 MB (32.16%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -2,26 +2,19 @@ class Solution:
     def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
         #this is a very easy and optimized approach by use of hashmap
         # as we can count the pattern and save it into map
-        h={} 
+        h=defaultdict(int)
 
         #iterate through each row 
         for i in matrix:
-            # row="" #string to store th pattern of matrix
+            row="" #string to store th pattern of matrix
 
-            # #for each element in the row ,check if it is same as first element
-            # for j in range(len(i)):
-            #     #"t" for same ,"f" for flipped
-            #     row+="t" if i[0]==i[j] else "f"
-
-            row=tuple(i)
-            flip=tuple(1-b for b in i)
-            n=min(row,flip)
+            #for each element in the row ,check if it is same as first element
+            for j in range(len(i)):
+                #"t" for same ,"f" for flipped
+                row+="t" if i[0]==i[j] else "f"
 
 #update the count in map
-            if n in h:
-                h[n]+=1
-            else:
-                h[n]=1
+            h[row]+=1
                        
     #return the max values of count in map
         return max(h.values())

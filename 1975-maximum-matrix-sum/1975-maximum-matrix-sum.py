2af0e412367a7bd34cf10221e51745a6fad0ba72
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-24 10:19:01 +0530

    Time: 79 ms (79.29%), Space: 25.1 MB (41.3%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.py
@@ -8,16 +8,27 @@ class Solution:
     
     #Maximum sum will be return in the last
 
-        t=0
-        min_abs=float('inf')
-        n=0
-        for i in matrix:
-            for val in i:
-                t+=abs(val)
-                min_abs=min(min_abs,abs(val))
-                if val<0:
+        t=0 #total sum
+        min_abs=float('inf') #this is used to track smallest absolute value of matrix
+        n=0 #counter for counting negative no. that are present in matrix
+        #traverse through each row of matrix
+        for r in matrix:
+            #Traverse through each element in row
+            for e in r:
+                #add abs value of current element(e) to total sum
+                t+=abs(e)
+                
+                #update smallest absolute element seen so far
+                min_abs=min(min_abs,abs(e))
+
+                #if the element is negative then increment n by 1
+                if e<0:
                     n+=1
+
+        #if there are an odd no. of negative, one will remain same
+        #subtract twice the smallest absolute elements to adjust the sum
         if n%2!=0:
             t-=2*min_abs
                     
+        #return the maximum sum of the matrix
         return t

4979600444c1014e95b852e74c37fef59f957bd2
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-11-26 19:12:30 +0530

    Time: 27 ms (90.52%), Space: 22 MB (63.39%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.py
@@ -0,0 +1,20 @@
+class RecentCounter:
+
+    def __init__(self):
+        #initialize a deque to store timestamps of recent requests
+        self.dq=deque()
+
+    def ping(self, t: int) -> int:
+        #add the current request timestamp to the deque
+        self.dq.append(t)
+
+        #remove timestamps that are outside the range(t-3000,t)
+        while self.dq[0]<t-3000:
+            self.dq.popleft() #remove the oldest timestamp from the range
+        #return the count of timestamps within the valid range
+        return len(self.dq) 
+
+
+# Your RecentCounter object will be instantiated and called as such:
+# obj = RecentCounter()
+# param_1 = obj.ping(t)

eeb43e6e1881244b6c1de725d49d0f497cfc5e38
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-09 01:07:09 +0530

    Time: 59 ms (60.3%), Space: 31.5 MB (22.55%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,12 +1,24 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        l=0
+        #we use sliding window approach in it
+        # l=0
+        # s=0
+        # m=float('-inf')
+        # for r in range(len(nums)):
+        #     s+=nums[r]
+        #     m=max(m,s)
+        #     if s<0:
+        #         s=0 #it reset the sum 
+        #         l=r+1
+        # return m
+
+#kadane algo
         s=0
-        m=float('-inf')
-        for r in range(len(nums)):
-            s+=nums[r]
+        m=float("-inf")
+        for i in nums:
+            s+=i
             m=max(m,s)
             if s<0:
-                s=0 #it reset the sum 
-                l=r+1
+                s=0
         return m
+            

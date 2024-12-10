23fc000e430436e6341feefdb21fb5ce614b0489
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-10 10:25:40 +0530

    Time: 47 ms (58.15%), Space: 46.7 MB (44.19%) - LeetHub

--- 3152-special-array-ii/3152-special-array-ii.py
+++ 3152-special-array-ii/3152-special-array-ii.py
@@ -1,17 +1,67 @@
 class Solution:
     def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
-        n=len(nums)
-        p=[0]*n #array to store prefix sum of alternating parity pairs
-        r=[] #for storing bool values
+        #collect index of violeting index
+        v=[]
+        for i in range(1,len(nums)):
+            if nums[i]%2==nums[i-1]%2:
+                v.append(i)
+        res=[]
 
-        for i in range(1,n): #build prefix array
-            if nums[i]%2!=nums[i-1]%2: #check if adjacent elements have different parity
-                p[i]+=1 #increment if they have different parity
-            p[i]+=p[i-1] #accumulate the value for prefix sum
+        #process each query
+        for s,e in queries:
+            s+=1
+            f=False
+            l=0
+            r=len(v)-1
 
-        for u,v in queries:
-            r.append(p[v]-p[u]==v-u) #true if all pairs are alternating
-        return r
+            #binary search to find if any index in "v" is in range of queries 
+            while l<=r:
+                mid=l+(r-l)//2
+                if v[mid]<s:
+                    l=mid+1
+                elif v[mid]>e:
+                    r=mid-1
+                else:
+                    f=True
+                    break
+
+            #if no violeting index is found, the subarray is special add into result
+            res.append(not f)
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    # def isArraySpecial(self, nums: List[int], queries: List[List[int]]) -> List[bool]:
+    #     n=len(nums)
+    #     p=[0]*n #array to store prefix sum of alternating parity pairs
+    #     r=[] #for storing bool values
+
+    #     for i in range(1,n): #build prefix array
+    #         if nums[i]%2!=nums[i-1]%2: #check if adjacent elements have different parity
+    #             p[i]+=1 #increment if they have different parity
+    #         p[i]+=p[i-1] #accumulate the value for prefix sum just like cummulative sum
+
+    #     #process each query
+    #     for u,v in queries:
+    #         #check if no. of alternating pairs in subarray matches the no. of pairs
+    #         r.append(p[v]-p[u]==v-u) #true if all pairs are alternating
+    #     return r
 
 
 

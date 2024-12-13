33e5db91cef821657b3e232388c6e2dc5a752aae
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-13 23:27:12 +0530

    Time: 525 ms (60%), Space: 36.8 MB (85.53%) - LeetHub

--- 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
+++ 2593-find-score-of-an-array-after-marking-all-elements/2593-find-score-of-an-array-after-marking-all-elements.py
@@ -1,23 +1,46 @@
 class Solution:
     def findScore(self, nums: List[int]) -> int:
-        n=len(nums)
-        #created a sorted ordered pair set of (value,index) pairs from nums
-        k=[(nums[i],i) for i in range(n)]
-        k.sort()
-        s=0 #score=0
-        visit=[False]*n
-        for v,i in k: #loop
-            if visit[i]:
-                continue
-            s+=v #add value to score
-            visit[i]=True
+        s=0 #Initialize score
+        heap=[(num,i) for i,num in enumerate(nums)] #create a list of tuples (num,index) 
+        heapify(heap) #convert it into min heap
+        marked=[False]*len(nums) #to keep track of marked indices
+        while heap:
+            num,i=heappop(heap) #pop element with the smallest no.
+            if not marked[i]: #if the index has not been visited yet
+                s+=num # add into score
 
-            #check and marked if the left neighbourif exists in set
-            if i-1>=0 and not visit[i-1]: 
-                visit[i-1]=True
+                #mark current ,left and right index visited
+                if i-1>=0:
+                    marked[i-1]=True #left
+                    
+                marked[i]=True       #current
 
-            #check and marked if the right neighbour exists in set
-            if i+1<n and not visit[i+1]:
-                visit[i+1]=True
+                if i+1<len(nums):
+                    marked[i+1]=True #right
+        return s
 
-        return s #return the score
+
+
+
+        
+        # n=len(nums)
+        # #created a sorted ordered pair set of (value,index) pairs from nums
+        # k=[(nums[i],i) for i in range(n)]
+        # k.sort()
+        # s=0 #score=0
+        # visit=[False]*n
+        # for v,i in k: #loop
+        #     if visit[i]:
+        #         continue
+        #     s+=v #add value to score
+        #     visit[i]=True
+
+        #     #check and marked if the left neighbourif exists in set
+        #     if i-1>=0 and not visit[i-1]: 
+        #         visit[i-1]=True
+
+        #     #check and marked if the right neighbour exists in set
+        #     if i+1<n and not visit[i+1]:
+        #         visit[i+1]=True
+
+        # return s #return the score

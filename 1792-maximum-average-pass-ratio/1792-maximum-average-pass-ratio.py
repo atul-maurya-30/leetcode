8f0632e8f347a4ce506ad7469a449253ac8f9c37
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-15 23:04:25 +0530

    Time: 1389 ms (13.33%), Space: 54.6 MB (18.52%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.py
@@ -1,38 +1,68 @@
 class Solution:
     def maxAverageRatio(self, classes: List[List[int]], extraStudents: int) -> float:
         r=len(classes)
-        a=[]
-        for i in range(r):
-            o=classes[i][0]/classes[i][1]
-            a.append(o)
-        while extraStudents!=0:
-            up_a=[]
-            for i in range(r):
-                new_o=(classes[i][0]+1)/(classes[i][1]+1)
-                up_a.append(new_o)
+        heap=[]
+
+        #calculate delta function to reduce the redundancy of code
+        def cal(c):
+            p,t=c
+            delta=((p+1)/(t+1))-(p/t) #find delta value
+            return delta
         
-            # it takes o(n)
-            # best=0
-            # best_delta=0.0
-            # for i in range(r):
-            #     delta=up_a[i]-a[i]
-            #     if delta>best_delta:
-            #         best_delta=delta
-            #         best=i
+        #intialize heap with delta for each classes
+        for i in range(r):
+            heapq.heappush(heap,(-cal(classes[i]),i)) #max heap by negative index
+
+        #distribute extra students to every classes
+        while extraStudents>0:
+            _,ind=heapq.heappop(heap) #get classes with maximum delta
+            classes[ind][0]+=1 #add a student
+            classes[ind][1]+=1 #update total class
+
+            #recalculate the delta
+            c=(classes[ind][0],classes[ind][1])
+            heapq.heappush(heap,(-cal(c),ind))
+            extraStudents-=1
+
+        #calculate the final average ratio
+        return sum(p/t for p,t in classes)/r
+        
             
 
-#so we use max heap
+# time limit exceeded
+#         r=len(classes)
+#         a=[]
+#         for i in range(r):
+#             o=classes[i][0]/classes[i][1]
+#             a.append(o)
+#         while extraStudents!=0:
+#             up_a=[]
+#             for i in range(r):
+#                 new_o=(classes[i][0]+1)/(classes[i][1]+1)
+#                 up_a.append(new_o)
+        
+#             # it takes o(n)
+#             # best=0
+#             # best_delta=0.0
+#             # for i in range(r):
+#             #     delta=up_a[i]-a[i]
+#             #     if delta>best_delta:
+#             #         best_delta=delta
+#             #         best=i
             
-            heap=[]
-            for i in range(r):
-                delta=up_a[i]-a[i]
-                heapq.heappush(heap,(-delta,i)) #push negative delta for max heap
-            _,best=heapq.heappop(heap)
-            a[best]=up_a[best]
-            classes[best][0]+=1
-            classes[best][1]+=1
-            extraStudents-=1
-        k=0
-        for i in range(r):
-            k+=a[i]
-        return k/r
+
+# #so we use max heap
+            
+#             heap=[]
+#             for i in range(r):
+#                 delta=up_a[i]-a[i]
+#                 heapq.heappush(heap,(-delta,i)) #push negative delta for max heap
+#             _,best=heapq.heappop(heap)
+#             a[best]=up_a[best]
+#             classes[best][0]+=1
+#             classes[best][1]+=1
+#             extraStudents-=1
+#         k=0
+#         for i in range(r):
+#             k+=a[i]
+#         return k/r

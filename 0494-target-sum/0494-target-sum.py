33d5d55f443c26cf7314f9e20bff4c13370f4474
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-26 23:29:22 +0530

    Time: 223 ms (11.48%), Space: 18.4 MB (59.08%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -11,18 +11,26 @@ class Solution:
 
     # def findTargetSumWays(self, nums: List[int], target: int) -> int:
     #     return self.solve(nums,0,0,target)
+
+
+    #using hash map
     def solve(self,nums,i,s,target,h):
         if i==len(nums):
             if s==target:
                 return 1
             return 0
-        k=f"{i}_{s}"
-        if k in h:
+        k=f"{i}_{s}" #create unique key for current state
+        
+        if k in h: #create if result is already save in hashmap
             return h[k]
+
+        #compute result recusively
         plus=self.solve(nums,i+1,s+nums[i],target,h)
         minus=self.solve(nums,i+1,s-nums[i],target,h)
+
+        #store result in hashmap as memoization
         h[k]=plus+minus
-        return h[k]
+        return h[k] #return result
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         h={}
         return self.solve(nums,0,0,target,h)

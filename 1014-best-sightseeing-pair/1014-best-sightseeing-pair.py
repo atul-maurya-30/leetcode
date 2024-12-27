d952faf256ffdccc9d75262c7a8fb102bfaf1946
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-27 23:10:43 +0530

    Time: 90 ms (14.39%), Space: 23.6 MB (5.12%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -1,14 +1,25 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        #optimized code after brute force
-        # we can see that only j is changing so we can store the value of maximum values till current in a variable m
+        # #optimized code after brute force
+        # # we can see that only j is changing so we can store the value of maximum values till current in a variable m
 
-        
+
+        # i=float("-inf") #for storing max value of values[i]+i
+        # max_m=float("-inf") #used for storing the maximum score
+        # for j in range(1,len(values)):
+        #     k=values[j]-j #it gives the current score of values[j]
+        #     l=values[j-1]+(j-1) #it gives the previous score of values[j-1] 
+        #     i=max(i,l) #update max value for ith
+        #     max_m=max(max_m,i+k) #update max value for the score
+        # return max_m
+
         i=float("-inf") #for storing max value of values[i]+i
         max_m=float("-inf") #used for storing the maximum score
+        heap=[]
         for j in range(1,len(values)):
             k=values[j]-j #it gives the current score of values[j]
             l=values[j-1]+(j-1) #it gives the previous score of values[j-1] 
-            i=max(i,l) #update max value for ith
+            heapq.heappush(heap,-l)
+            i=-heap[0]
             max_m=max(max_m,i+k) #update max value for the score
         return max_m

6122f3b4c83ec7ec33d07240fcb5140d459f9266
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-27 22:24:11 +0530

    Time: 78 ms (27.07%), Space: 23.2 MB (24.63%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.py
@@ -1,13 +1,14 @@
 class Solution:
     def maxScoreSightseeingPair(self, values: List[int]) -> int:
-        m=float("-inf")
-        memo=set()
-        for i in range(len(values)):
-            for j in range(i,len(values)):
-                if (i,j) in memo:
-                    continue
-                if i<j:
-                    s=values[i] + values[j] + i - j
-                    m=max(m,s)
-                    memo.add((i,j))
-        return m
+        #optimized code after brute force
+        # we can see that only j is changing so we can store the value of maximum values till current in a variable m
+
+        
+        i=float("-inf") #for storing max value of values[i]+i
+        max_m=float("-inf") #used for storing the maximum score
+        for j in range(1,len(values)):
+            k=values[j]-j #it gives the current score of values[j]
+            l=values[j-1]+(j-1) #it gives the previous score of values[j-1] 
+            i=max(i,l) #update max value for ith
+            max_m=max(max_m,i+k) #update max value for the score
+        return max_m

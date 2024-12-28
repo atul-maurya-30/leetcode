54982ec7819f650cc5937155e39507aeabf681ca
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-28 13:25:48 +0530

    Time: 212 ms (10.28%), Space: 33.2 MB (9.72%) - LeetHub

--- 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.py
@@ -1,32 +1,39 @@
 class Solution:
     #we need helper function that count the value of maximum sum of subarrays
-    def helper(self,i,c,s,k):
+    def helper(self,i,c,s,k,memo):
         if c==0:                                    #|for count
             return 0                            
                                                     #| BASE CASE
         if i>=len(s):                               #|for overbound we got invalid value so for counter that part we have to give the minimum value for the helper function      
-            return float("-inf")           
+            return float("-inf") 
 
-        take=s[i]+self.helper(i+k,c-1,s,k) #recall the function for taking the subarray with maximum value
-        not_take=self.helper(i+1,c,s,k) #recall function for skipping subarrays 
-        return max(take,not_take) #return max value among take or not_take
+        #check if current index is already computed
+        if (i,c) in memo:
+            return memo[(i,c)]
+
+        take=s[i]+self.helper(i+k,c-1,s,k,memo) #recall the function for taking the subarray with maximum value
+        not_take=self.helper(i+1,c,s,k,memo) #recall function for skipping subarrays 
+
+        #store result into memo
+        memo[(i,c)]=max(take,not_take) #max value among take or not_take
+        return  memo[(i,c)]#return max value
 
     #core part of the code
-    def solve(self,s,c,i,k,res): #s=subrray;c=3;i=current index;k=total subarray length;res:for storing the index of maximum subarrays
+    def solve(self,s,c,i,k,res,memo): #s=subrray;c=3;i=current index;k=total subarray length;res:for storing the index of maximum subarrays
         if c==0:return #base case for count
         if i>=len(s):return #base case for overbound
 
-        take=s[i]+self.helper(i+k,c-1,s,k) #for picking current subarray
-        not_take=self.helper(i+1,c,s,k) #for skipping current subarray by indexing +1
+        take=s[i]+self.helper(i+k,c-1,s,k,memo) #for picking current subarray
+        not_take=self.helper(i+1,c,s,k,memo) #for skipping current subarray by indexing +1
 
         #for deciding whether to pick or skip array and store 0th index of subarray into res    
         if take>=not_take:
             res.append(i)
             #move forward to next subarray after selecting current
-            self.solve(s,c-1,i+k,k,res)
+            self.solve(s,c-1,i+k,k,res,memo)
         else:
             #move forward if current subarray is not maximum
-            self.solve(s,c,i+1,k,res)
+            self.solve(s,c,i+1,k,res,memo)
 
     def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
         #we use sliding window approach for storing the value of subarrays sum according to the value of k
@@ -42,5 +49,6 @@ class Solution:
             j+=1
         #intialise res for storing the 0th index of subarrays
         res=[]
-        self.solve(sub,3,0,k,res)
+        memo={} #initialize for memoization
+        self.solve(sub,3,0,k,res,memo)
         return res

3518f68d78510387d71239c6649ca1af785135ba
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2024-12-30 01:37:50 +0530

    Time: 0 ms (100%), Space: 17.7 MB (18.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,10 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        if m>n:
-            return (m*n)-m
-        elif m<n:
-            return (m*n)+n
-        else:
-            return 0
-
+        #total moves required are m-1 down moves and n-1 right moves
+        #combination formula C(m+n-2,m-1), compute the no. of ways to arrange these moves
+        return math.comb(((m-1)+(n-1)),n-1) #can use any one (m-1)/(n-1)
                 

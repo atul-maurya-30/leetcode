09966d28973931f92eac31e4ed9e944f9e4b7724
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2025-01-05 19:47:25 +0530

    Time: 104 ms (47.37%), Space: 30.7 MB (7.89%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.py
+++ 0848-shifting-letters/0848-shifting-letters.py
@@ -1,11 +1,13 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
         n=len(s)
+        #if you see clearly then it just uses the sumulative sum in reverse order
         r=shifts[::-1]
         for i in range(1,len(r)):
             r[i]+=r[i-1]
         k=r[::-1]
         s=list(s)
+        #update values
         for i in range(n):
             v=ord(s[i])-ord("a")
             z=(v+k[i])%26

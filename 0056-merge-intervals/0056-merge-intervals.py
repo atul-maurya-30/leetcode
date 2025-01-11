45ca55b931409ab6f946a9d5945ddabbacdd7d1b
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2025-01-11 18:48:51 +0530

    Time: 7 ms (74.37%), Space: 20.5 MB (64.03%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,14 +2,12 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         s=[]
         intervals.sort() #sort the interval in order
-        # if len(intervals)==1: #check for base case
-        #     return intervals
-        for i in range(1,len(intervals)):
-            a=intervals[i-1][1]
-            b=intervals[i][0]
-            if a>=b:
-                intervals[i]=[intervals[i-1][0],max(intervals[i-1][1],intervals[i][1])]
+        for i in range(1,len(intervals)): 
+            a=intervals[i-1][1] #start
+            b=intervals[i][0]   #final
+            if a>=b: #if final of current is less than or equAL to start of first 
+                intervals[i]=[intervals[i-1][0],max(intervals[i-1][1],intervals[i][1])] #update the current element
             else:
-                s.append(intervals[i-1])
-        s.append(intervals[-1])
-        return s
+                s.append(intervals[i-1]) #add the previous element
+        s.append(intervals[-1]) #and atlast add the current element
+        return s #return the new list of intervals

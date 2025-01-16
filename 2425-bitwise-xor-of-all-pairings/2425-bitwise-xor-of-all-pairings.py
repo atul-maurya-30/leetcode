11a3a3f32f268848edab70be730c71cbd50adc47
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2025-01-16 22:30:28 +0530

    Time: 19 ms (13.29%), Space: 36.8 MB (12.72%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -1,10 +1,27 @@
 class Solution:
     def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
-        a=[]
-        for i in range(len(nums1)):
-            for j in range(len(nums2)):
-                a.append(nums1[i]^nums2[j])
-        c=0
-        for i in range(len(a)):
-            c^=a[i]
-        return c
+        # a=[]
+        # for i in range(len(nums1)):
+        #     for j in range(len(nums2)):
+        #         a.append(nums1[i]^nums2[j])
+        # c=0
+        # for i in range(len(a)):
+        #     c^=a[i]
+        # return c
+
+        #since here brute force is not work
+        #so we have to use the concept of properties of xor for the even or odd if the identities is same
+        x1=0
+        x2=0
+
+        for i in range(len(nums1)): #store the values of xor of nums1 in x1
+            x1^=nums1[i]
+        for i in range(len(nums2)): #store the values of xor of nums2 in x2
+            x2^=nums2[i]
+                
+        res=0
+        if len(nums2)%2==1: #if length of nums2 is odd then take a xor of x1
+            res^=x1 
+        if len(nums1)%2==1: #if length of nums1 is odd then take a xor of x2
+            res^=x2
+        return res #return result

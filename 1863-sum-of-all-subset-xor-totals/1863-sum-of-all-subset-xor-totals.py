bba1725a8e6e28205452039c9698b1cb418f52f2
Atul Maurya <119139143+atul-maurya-30@users.noreply.github.com>
2025-04-05 18:18:12 +0530

    Time: 0 ms (100%), Space: 16.6 MB (100%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.py
@@ -1,6 +1,11 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
-        r=0
+        r=0 #to store cummulative or of all no.
+
+        # iterate through each no. in the arr
         for n in nums:
+            #perform bitwise OR operation ,this ensures that all bits present in no. are inlcuded in r
             r|=n
+
+        # multiply by subset count contribution
         return r*(2**(len(nums)-1))
